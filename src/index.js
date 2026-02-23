92fbccc2f259753a115970552c85fcca9733f463
Momo <moharryg@gmail.com>
2026-02-23 12:58:22 +0000

    fixed permisson and added to the whitelist

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const eventHandler = require('./handlers/eventHandler');
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
     ]
